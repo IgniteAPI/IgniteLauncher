4db0ff088443b7a3b89a51f62076b08c7b1efe7e
Garrett <52760019+Casimir255@users.noreply.github.com>
2025-09-14 15:48:25 -0500

    Update project for webAPI

--- Ignite/Program.cs
+++ Ignite/Program.cs
@@ -1,17 +1,36 @@
-﻿
+
 namespace Ignite
 {
-    internal class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = WebApplication.CreateBuilder(args);
 
-            
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
 
 
-           
+            app.MapControllers();
 
+            Console.WriteLine("Hello world!");
+
+            app.Run();
         }
     }
 }
